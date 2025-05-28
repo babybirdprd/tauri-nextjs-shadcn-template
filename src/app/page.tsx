7fe31b0219db4bcb1f21e8a5470d61dd959f729a
babybirdprd <babybirdprd@gmail.com>
2025-05-27 22:34:10 -0400

    added dark mode toggle and tweakcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { invoke } from "@tauri-apps/api/core";
 import Image from "next/image";
 import { useCallback, useState } from "react";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   const [greeted, setGreeted] = useState<string | null>(null);
@@ -17,7 +18,10 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="relative grid grid-rows-[auto_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
